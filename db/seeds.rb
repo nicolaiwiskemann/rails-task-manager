9049c71b1a813468f7cd623e7256782bc66aabf1
Nicolai Wiskemann <nicolai.wiskemann@gmail.com>
2017-02-14 13:26:02 +0000

    fini9shed the app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(name: "HSV", description: "ist der beste verein" )
+Task.create(name: "bayern", description: "ist beschissen" )
+Task.create(name: "bremen", description: "stinkt" )
